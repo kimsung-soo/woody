757c216e1f737aa58029a987a50c4ba1afbffe47
Choi Eunsu <zxzx320@naver.com>
2025-08-13 09:41:33 +0900

    ㅡㅡ

--- server/routers/martarials_router.js
+++ server/routers/martarials_router.js
@@ -2,14 +2,9 @@ const express = require("express");
 const router = express.Router();
 const matarialService = require("../services/materials_service");
 
-// router.get("/matarials", async (req, res) => {
-//   let matarialList = await matarialService.matarialSelect();
-//   res.send(matarialList);
-// });
-
-router.get("/materials", async (req, res) => {
-  let list = await matarialService.materialfindAll();
-  res.send(list);
+router.get("/matarials", async (req, res) => {
+  let matarialList = await matarialService.matarialSelect();
+  res.send(matarialList);
 });
 
 module.exports = router;
